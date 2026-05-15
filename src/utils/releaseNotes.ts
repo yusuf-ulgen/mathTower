b87cf0baa7ef8a85849115a2f68e4b25d34a2d1a
Yusuf Ulgen <ysfulgen142@gmail.com>
2026-05-15 21:39:46 +0300

    chore: sürüm yükseltildi.

--- src/utils/releaseNotes.ts
+++ src/utils/releaseNotes.ts
@@ -2,6 +2,6 @@
 // Her güncellemede sadece buradaki 'notes' kısmını ve gerekiyorsa 'version' kısmını değiştirmeniz yeterlidir.
 
 export const APP_RELEASE_NOTES = {
-  version: "1.0.1",
-  notes: "1) Yeni Bölümler: Savaşa 5 yeni zorlu kule kalesi eklendi.\n2) Görsel İyileştirmeler: Kuleler ve askerler için daha detaylı kaplamalar eklendi.\n3) Performans: Büyük savaşlarda yaşanan performans sorunları optimize edildi.\n4) Hata Düzeltmeleri: Matematik kapılarındaki bazı hesaplama hataları giderildi."
+  version: "1.0.6",
+  notes: "1) Yeni Bölümler: Savaşa 5 yeni zorlu kule eklendi.\n2) Artık dost kulelere de birlikler konuşlandırılabilecek.\n3) Performans: Büyük savaşlarda yaşanan performans sorunları optimize edildi.\n4) Hata Düzeltmeleri: Matematik kapılarındaki bazı hesaplama hataları giderildi."
 };
